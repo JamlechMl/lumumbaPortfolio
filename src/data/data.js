d33c6d30411799ecc85a43a994b99b6d769d8018
JamlechMl <jamlechmukavana601@gmail.com>
2022-11-21 14:26:11 +0300

    react-create-app on Heroku

---
+++ src/data/data.js
@@ -0,0 +1,38 @@
+import Class from '../assets/projects/class.jpg';
+import Forest from '../assets/projects/forest.jpg';
+import Hike from '../assets/projects/hike.jpg';
+import myself from '../assets/projects/myself.jpg';
+import Stone from '../assets/projects/stones.jpg';
+
+export const data = [
+  {
+    id: 1,
+
+    image: myself
+  },
+  {
+    id: 2,
+
+    image: Hike
+  },
+  {
+    id: 3,
+
+    image: Hike
+  },
+  {
+    id: 4,
+
+    image: Stone
+  },
+  {
+    id: 5,
+
+    image: Class
+  },
+  {
+    id: 5,
+
+    image: Forest
+  }
+];
